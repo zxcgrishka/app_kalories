107cb6ed3c43ba4bda66054563f46f53ad0a3d4f
grishka <kolyackaam@gmail.com>
2025-12-03 22:18:42 +0300

    added tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,4 +89,8 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    testImplementation(libs.junit)
 }
